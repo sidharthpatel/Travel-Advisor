77cf55bdb59ece92363912aefd2c5596fab16b8f
Siddharth Patel <sidharthpatel012@gmail.com>
2022-05-06 19:50:00 -0400

    Currently working on generating a header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
     return(
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        // React Fragment
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{width: '100%', background: 'blue'}} >
+                {/* xs={12} App will have full-width on small screens such as mobile devices.
+                    md={4}  will take 4/12 width on medium screens such as tablets */}
+                <Grid item xs={12} md={4} style={{background: 'red'}} >
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8} >
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AutoComplete } from "@react-google-maps/api";
+import { AppBar, ToolBar, Typography, InputBase, Box } from "@material-ui/core";
+
+const Header = () => {
+  return(
+    <AppBar>
+      
+    </AppBar>
+  )
+};
+
+export default Header;
