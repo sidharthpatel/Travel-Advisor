3d9cf665842d9e08219a426992c9461f92959764
Siddharth Patel <sidharthpatel012@gmail.com>
2022-05-11 22:34:12 -0400

    Generated the Map component which presents a google map on the website.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { CssBaseline, Grid } from '@material-ui/core';
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
-    return(
+    return (
         // React Fragment
         <>
             <CssBaseline />
             <Header />
-            <Grid container spacing={3} style={{width: '100%', background: 'blue'}} >
+            <Grid container spacing={3} style={{ width: "100%" }}>
                 {/* xs={12} App will have full-width on small screens such as mobile devices.
                     md={4}  will take 4/12 width on medium screens such as tablets */}
-                <Grid item xs={12} md={4} style={{background: 'red'}} >
+                <Grid item xs={12} md={4}>
                     <List />
                 </Grid>
-                <Grid item xs={12} md={8} >
+                <Grid item xs={12} md={8}>
                     <Map />
                 </Grid>
             </Grid>
         </>
-    )
-}
+    );
+};
 
 export default App;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-    return(
-        <h1>Map</h1>
-    )
-}
+    const classes = useStyles();
+
+    /* isMobile var will be set to false if the width of the device is greater than 600 px. */
+    const isMobile = useMediaQuery("(min-width: 600px)");
+
+    const coordinates = { lat: 0, lng: 0 };
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: "AIzaSyBk3csiK2-tXjbZkuxXimgygDzvh2EFgas",
+                }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            ></GoogleMapReact>
+        </div>
+    );
+};
 
 export default Map;
